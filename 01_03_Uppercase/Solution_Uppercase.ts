7470d3fd6941e0aa661be4972634065728c55ad4
David Lorenz <activenode@users.noreply.github.com>
2023-04-13 08:22:12 +0000

    Solution added

---
+++ 01_03_Uppercase/Solution_Uppercase.ts
@@ -0,0 +1,43 @@
+type Alphabet =
+  | "a"
+  | "b"
+  | "c"
+  | "d"
+  | "e"
+  | "f"
+  | "g"
+  | "h"
+  | "i"
+  | "j"
+  | "k"
+  | "l"
+  | "m"
+  | "n"
+  | "o"
+  | "p"
+  | "q"
+  | "r"
+  | "s"
+  | "t"
+  | "u"
+  | "v"
+  | "w"
+  | "x"
+  | "y"
+  | "z";
+
+type GermanUppercaseStrings = `${Uppercase<Alphabet>}${string}`;
+
+// komplexe Lösung möglich, aber nicht notwendig
+
+export const Output = (s: GermanUppercaseStrings) => {
+  console.log(s);
+};
+
+Output("sabrina"); // ❌
+Output(23); // ❌
+Output("23"); // ❌
+Output("mark"); // ❌
+Output(" Jessy"); // ❌
+Output("Jess"); // ✅
+Output("David"); // ✅
