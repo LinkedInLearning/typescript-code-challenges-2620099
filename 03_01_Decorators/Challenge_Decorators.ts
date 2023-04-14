c2bdbb11092064e1f0933e0a44f802e5b4bbae99
David Lorenz <activenode@users.noreply.github.com>
2023-04-14 08:39:45 +0000

    updated path

--- 03_01_Decorators/Challenge_Decorators.ts
+++ 03_01_Decorators/Challenge_Decorators.ts
@@ -1,6 +1,3 @@
-// Level 3
-
-// https://devblogs.microsoft.com/typescript/announcing-typescript-5-0/#decorators
 function jsonToObject(originalMethod: any, context: any) {
   return function decoratorFunction(this: any, ...args: any[]) {
     return JSON.parse(originalMethod(...args));
